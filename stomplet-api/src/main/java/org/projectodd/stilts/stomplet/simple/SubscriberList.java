bfc6cf8b52f11a9c65dd847994370bb4b61327e1
kangsik <kangsik.jung@gmail.com>
2014-07-16 15:27:16 +0900

    SubscriberList의 구현을 변경하여 성능 확보

--- stomplet-api/src/main/java/org/projectodd/stilts/stomplet/simple/SubscriberList.java
+++ stomplet-api/src/main/java/org/projectodd/stilts/stomplet/simple/SubscriberList.java
@@ -16,9 +16,10 @@
 
 package org.projectodd.stilts.stomplet.simple;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Collection;
+import java.util.Iterator;
 import java.util.Random;
+import java.util.concurrent.ConcurrentLinkedQueue;
 
 import org.jboss.logging.Logger;
 import org.projectodd.stilts.stomp.StompException;
@@ -28,39 +29,56 @@ import org.projectodd.stilts.stomplet.Subscriber;
 public class SubscriberList {
 
     private static Logger log = Logger.getLogger(SubscriberList.class);
+    private static boolean perfWarned = false;
     
     public SubscriberList() {
     }
 
-    public synchronized int size() {
+    public int size() {
         return this.subscribers.size();
     }
 
-    public synchronized void addSubscriber(Subscriber subscriber) {
+    public void addSubscriber(Subscriber subscriber) {
         this.subscribers.add( subscriber );
     }
 
-    public synchronized boolean removeSubscriber(Subscriber subscriber) {
+    public boolean removeSubscriber(Subscriber subscriber) {
         return this.subscribers.remove( subscriber );
     }
 
-    protected synchronized void sendToAllSubscribers(StompMessage message) throws StompException {
-        for (Subscriber each : this.subscribers) {
+    protected void sendToAllSubscribers(StompMessage message) throws StompException {
+        Iterator<Subscriber> iter = subscribers.iterator();
+        while (iter.hasNext()) {
+    		Subscriber each = iter.next();
         	try {
         		each.send( message );
         	} catch (StompException se) {
         		log.warn("다음 subscriber에게 메시지를 전달하는 중 예외가 발생하였습니다. subscriber: " + each, se);
-        		removeSubscriber(each);
+        		iter.remove();
         	}
         }
     }
 
     protected synchronized void sendToOneSubscriber(StompMessage message) throws StompException {
+    	// sendToOneSubscriber()는 사용되지 않을 것이라 가정하고 sendToAllSubscribers의 lock-free 구현을 위해
+    	// ConcurrentLinkedQueue를 사용함
+    	if (!perfWarned) {
+        	log.warn("비효율적으로 구현된 메소드가 호출되었습니다. 이 메소드를 사용하려면 구현을 변경하세요!");	
+    		perfWarned = true;
+    	}
         int luckyWinner = this.random.nextInt( this.subscribers.size() );
-        this.subscribers.get( luckyWinner ).send( message );
+        Iterator<Subscriber> iter = subscribers.iterator();
+        int i = 0;
+        Subscriber subscriber = null;
+        while (i < luckyWinner && iter.hasNext()) {
+        	subscriber = iter.next();
+        }
+        if (subscriber != null) {
+        	subscriber.send( message );
+        }
     }
 
-    private final List<Subscriber> subscribers = new ArrayList<Subscriber>();
+    private final Collection<Subscriber> subscribers = new ConcurrentLinkedQueue<Subscriber>();
     private Random random = new Random( System.currentTimeMillis() );
 
 }
